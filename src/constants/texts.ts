a71b1bcfb67507e6063c705924669ba70abf7a04
Capellinx <capellaaa7@gmail.com>
2024-07-28 22:58:33 -0300

    faet: create texts to use in application

---
+++ src/constants/texts.ts
@@ -0,0 +1,61 @@
+
+export const aboutMe = `
+Meu nome é Lucas Capella, mas se preferir,
+pode me chamar de Capella (é como as pessoas
+costumam me chamar). Sou um desenvolvedor
+apaixonado por tecnologia e conhecimento,
+sempre buscando estar atualizado com as últimas
+tendências e inovações da área. Com mais de 2
+anos de experiência em desenvolvimento de
+software, sou especialista em implementar
+designs fiéis ao protótipo, garantindo
+desempenho e usabilidade para uma experiência
+fluida ao usuário.
+
+Tenho um grande interesse em práticas que
+fomentam a qualidade dos sistemas, como
+testes automatizados, Clean Architecture,
+monitoramento e observabilidade. Durante
+minha carreira, tive a oportunidade de
+trabalhar em diversos projetos acadêmicos e
+profissionais, o que me permitiu refinar minhas
+habilidades como desenvolvedor. Sou
+comprometido com a criação de sites de alta
+performance e otimização, implementando testes
+e utilizando lógica e arquitetura de software
+sólidas.
+`
+export const skills = [
+   'Typescript',
+   'React',
+   'NextJS',
+   'Tailwind',
+   'NodeJS',
+   'Docker',
+   'Express/Fastify',
+   'PostgreSQL',
+   'Prisma'
+]
+
+export const experience = [
+   {
+      title: 'Desenvolvedor Full Stack',
+      company: "VBA Systems",
+      description: "",
+      date: '2024 - Atualmente',
+   },
+   {
+      title: 'Desenvolvedor Front end',
+      company: "ICMBio Alacatrazes",
+      description: "",
+      date: '2022 - 2024',
+   },
+   {
+      title: 'Estagiário de desenvolvimento',
+      company: "ICMBio Alacatrazes",
+      description: "",
+      date: '2021 - 2022',
+   },
+]
+
+export const projects = [{}] 
