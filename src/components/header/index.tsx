df919e05aeec0990f8f7da2a84cd2cebb571f3a9
Capellinx <capellaaa7@gmail.com>
2024-07-28 22:10:56 -0300

    feat: create header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,18 @@
+
+
+
+export default function Header() {
+ return (
+   <header>
+      <h1>Lucas Capella</h1>
+      <nav>
+         <ul>
+            <li>Início</li>
+            <li>Sobre mim</li>
+            <li>Experiências</li>
+            <li>Projetos</li>
+         </ul>
+      </nav>
+   </header>
+ );
+}
