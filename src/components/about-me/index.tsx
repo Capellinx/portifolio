453c92486de8b037ec600197907a1fc5b64c716c
Capellinx <capellaaa7@gmail.com>
2024-07-28 22:50:45 -0300

    feat: create section about-me

---
+++ src/components/about-me/index.tsx
@@ -0,0 +1,21 @@
+export default function AboutMe() {
+   return (
+      <section>
+         <h1>Sobre mim</h1>
+         <div>
+            <span>São Paulo, Brasil</span>
+            <p>
+               Meu nome é Lucas Capella, mas se preferir pode me chamar de Capella (é como as pessoas costumam me chamar). Sou um desenvolvedor apaixonado por tecnologia e conhecimento, sempre buscando estar atualizado com as últimas tendências e inovações da área. Com mais de 2 anos de experiência em desenvolvimento de software, sou especialista em implementar designs fiéis ao protótipo, garantindo desempenho e usabilidade para uma experiência fluida ao usuário.
+
+               Tenho um grande interesse em práticas que fomentem a qualidade dos sistemas, como testes automatizados, Clean Architecture, monitoramento e observabilidade. Durante minha carreira, tive a oportunidade de trabalhar em diversos projetos acadêmicos e profissionais, o que me permitiu refinar minhas habilidades como desenvolvedor. Sou comprometido com a criação de sites de alta performance e otimização, implementando testes e utilizando lógica e arquitetura de software sólidas.
+            </p>
+         </div>
+         <nav>
+            <li>Linkedin</li>
+            <li>Instagram</li>
+            <li>Twitter</li>
+         </nav>
+         <button>Baixar currículo</button>
+      </section>
+   );
+}
